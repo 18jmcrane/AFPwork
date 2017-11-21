38b5157f5cd10912a801f5721dffb676f632dee8
18jmcrane <18jmcrane@mlmc.vic.edu.au>
2017-11-21 13:00:56 +1100

    working with int in using "+" and ","

--- GCcontent.py
+++ GCcontent.py
@@ -1,4 +1,5 @@
-sequence = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
+print("This will calculate the GC content of a DNA sequence")
+sequence = input("Enter your DNAsequence to calculate the GCcontent: ")
 #counts total of letters of string to be used to divide
 total = len(sequence)
 #counts number of c in string to calculate the percantages
@@ -16,4 +17,5 @@ percentageG = ((numberG/total) * 100)
 GCpercentage = (percentageG+percentageC)
 
 #prints and rounds it to ",2" 2 decimal places using the "round" funtion
-print(round(GCpercentage,2))
+Final = (round(GCpercentage,2))
+print("The GC content of your DNA sequence is", Final)
