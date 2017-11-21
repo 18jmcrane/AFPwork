7757b1b0d5b456e334f3ba462de380f31f80d874
18jmcrane <18jmcrane@mlmc.vic.edu.au>
2017-11-21 12:38:11 +1100

    working with int in using "+" and ","

--- test2.py
+++ test2.py
@@ -1,3 +1,11 @@
 print("Helloworld")
 
 name = input("name?")
+age = int(input("Enter age?: "))
+
+print("hello", name, age)
+
+#has to be converted into a string first
+print("hello" + name + str(age))
+
+print('hello {0} you are {1} years old'.format(name, age))
